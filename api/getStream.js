7768c499cb38398e5c5b8f1985c7d704d3ae61dd
deformed-steve <koltnapier2014@gmail.com>
2025-08-29 17:47:38 -0400

    Update getStream.js

--- api/getStream.js
+++ api/getStream.js
@@ -3,7 +3,9 @@ import ytdl from "@distube/ytdl-core";
 export default async function handler(req, res) {
   try {
     const { url } = req.query;
-    if (!url) return res.status(400).json({ error: "Missing ?url=" });
+    if (!url) {
+      return res.status(400).json({ error: "Missing ?url=" });
+    }
 
     const info = await ytdl.getInfo(url);
     const format = ytdl.chooseFormat(info.formats, {
